aced76ef8f87ba9feb57a2a4519f7724d327b32c
Mustafa1292 <35036281+Mustafa1292@users.noreply.github.com>
2021-10-05 14:50:44 -0500

    Add files via upload

---
+++ src/components/Animation.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Spray from "../client images/spray.png";
+
+const Animation = () => {
+  return (
+    <div>
+      <p>
+        hi
+        <img src={Spray} alt="spray" />;
+      </p>
+    </div>
+  );
+};
+
+export default Animation;
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import styled from 'styled-components'
+
+// const Container = styled.div``
+// const Wrapper = styled.div``
+// const FormContainer = styled.div``
+// const AddressContainer = styled.div``
+
+// const Contact = () => {
+//     return (
+//         <Container>
+
+//         </Container>
+//     )
+// }
+
+// export default Contact
